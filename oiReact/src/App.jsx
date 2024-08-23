83e484380f95f7ca13f702f6993bcaddc1716918
josehvinii <jvictordauzaker@gmail.com>
2024-08-23 17:16:15 -0300

    Criação do primeiro projeto com React bonitão

---
+++ oiReact/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Dinossauros</h1>
+      <h2>Carnívoros</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam suscipit maxime sed excepturi porro eos nulla esse iusto, ex consequatur, omnis corrupti nisi. Iste obcaecati eveniet saepe quidem. Minima, soluta?</p>
+    </>
+  )
+}
+
+export default App
