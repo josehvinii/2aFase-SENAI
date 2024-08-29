ce01542d9b97b4dcdb2d0f4a50001cc59857e6fa
josehvinii <jvictordauzaker@gmail.com>
2024-08-29 17:25:54 -0300

    add componentes

---
+++ componentes/componentesDeVerdade/src/components/Cabecalho.jsx
@@ -0,0 +1,11 @@
+import './Cabecalho.css'
+
+function Cabecalho() {
+  return (
+    <div className='Cabecalho-container'>
+      <h1>Treinar dragão</h1>
+    </div>
+  )
+}
+
+export default Cabecalho
