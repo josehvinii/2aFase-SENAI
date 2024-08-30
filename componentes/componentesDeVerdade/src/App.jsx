ea51668f05288470a1766223c245b91d223c9f2f
josehvinii <jvictordauzaker@gmail.com>
2024-08-30 17:12:08 -0300

    Atualização do repositório

--- componentes/componentesDeVerdade/src/App.jsx
+++ componentes/componentesDeVerdade/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Cabecalho from './components/Cabecalho'
+import Corpo from './components/Corpo'
 import Links from './components/Links'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
      <Cabecalho />
      <Links />
+     <Corpo />
     </>
   )
 }
---
+++ componentes/componentesDeVerdade/src/components/Corpo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Corpo() {
+  return (
+    <div>
+      <h2>Fantasma da Neve:</h2>
+      <img src="./images/dragao.png" alt="" />
+    </div>
+  )
+}
+
+export default Corpo
