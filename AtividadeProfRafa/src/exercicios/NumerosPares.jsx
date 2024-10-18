2e7a164e35b0d631bcf6661a0359132a6c046fb5
josehvinii <jvictordauzaker@gmail.com>
2024-10-18 14:47:53 -0300

    atividadesRafa2.0

--- AtividadeProfRafa/src/exercicios/NumerosPares.jsx
+++ AtividadeProfRafa/src/exercicios/NumerosPares.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import Navbar from '../components/Navbar'
 
 function NumerosPares() {
+  const [inputNumero, setNumero] = useState('')
+  const [contagem, setContagem] = useState(0)
+  const [Mostrar, setMostrar] = useState(false);
+
+  function lerNumero(){
+    if (inputNumero > 100 && inputNumero < 200){
+        setContagem(contagem + 1)
+    }
+}
+
+
+
+
+
   return (
     <div>
-      
+      <Navbar />
+
+      <h1>Exercício 7.13</h1>
+      <p>Digite dois números para mostrar os números pares contidos entre eles:</p>
+      <input type="number"
+      value={inputNumero}
+      onChange={(event) => setNumero(event.target.value)}
+      />
+      <button>Ler</button>
+      <button>Resultado</button>
     </div>
   )
 }
